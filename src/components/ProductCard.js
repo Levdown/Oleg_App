afeec49b20c4178ba67898281be8e67a689e5cb0
Лев Низковский <levniz@MacBook-Air-Lev.local>
2024-01-31 22:11:04 +0300

    test

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -42,7 +42,7 @@ function ProductCard(props){
                 <p className='CartTitle'>{product.name}</p>
                 {product.space ?
                 <>
-                    <span style={{color: 'black'}}>{product.space}</span>
+                    <span style={{color: 'black', fontSize: "0.7rem"}}>{product.space}</span>
                 </>
                 :
                 <>
