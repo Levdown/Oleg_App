8054727bd08b6d512062071a54fef42c76387057
Лев Низковский <levniz@MacBook-Air-Lev.local>
2024-02-13 22:59:12 +0300

    showprogress

--- src/products.js
+++ src/products.js
@@ -88,7 +88,7 @@ const dataProducts = [
     {
         id: 6,
         name: "Органическая СЫВОРОТКА ПИТЬЕВАЯ из молока коров породы Джерси. 0.5 л.",
-        price: 60,
+        price: 65,
         photo: photo6,
     },
 
@@ -96,7 +96,7 @@ const dataProducts = [
     {
         id: 7,
         name: "Органические СЛИВКИ ПИТЬЕВЫЕ 25% из молока коров породы Джерси. 0.5 л.",
-        price: 60,
+        price: 65,
         photo: photo7,
     },
     {
